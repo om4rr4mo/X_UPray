2f499c927fa06ac7b0228fa8e710b2a8d2c4b454
moussakareGueye <m.gueye@amdistributori.it>
2022-01-14 10:46:17 +0100

    [mg] flutter updated 2.8.1

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -158,7 +158,7 @@ class _HomePageState extends State<HomePage> {
                   pageController: pageController,
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),

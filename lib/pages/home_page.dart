d044b103eb36fc61eb90dec1b8e22c58d1758c13
mose98 <moussa98.mkg@gmail.com>
2022-02-06 04:14:14 +0100

    [mo][removed] hi man

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
 
-import 'package:android_intent/android_intent.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:prayers/pages/prayers_page.dart';
 import 'package:prayers/pages/qibla_page.dart';
 import 'package:prayers/pages/quran_pages.dart';
@@ -71,50 +69,12 @@ class _HomePageState extends State<HomePage> {
   //   return granted;
   // }
 
-  /*Show dialog if GPS not enabled and open settings location*/
-  Future _checkGps() async {
-    if (!(await Geolocator().isLocationServiceEnabled())) {
-      if (Theme.of(context).platform == TargetPlatform.android) {
-        showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: Text("Can't get gurrent location"),
-                content:
-                    const Text('Please make sure you enable GPS and try again'),
-                actions: <Widget>[
-                  FlatButton(
-                      child: Text('Ok'),
-                      onPressed: () {
-                        final AndroidIntent intent = AndroidIntent(
-                            action:
-                                'android.settings.LOCATION_SOURCE_SETTINGS');
-                        intent.launch();
-                        Navigator.of(context, rootNavigator: true).pop();
-                        _gpsService();
-                      })
-                ],
-              );
-            });
-      }
-    }
-  }
-
-  /*Check if gps service is enabled or not*/
-  Future _gpsService() async {
-    if (!(await Geolocator().isLocationServiceEnabled())) {
-      _checkGps();
-      return null;
-    } else
-      return true;
-  }
 
   @override
   void initState() {
     super.initState();
 
     //requestLocationPermission();
-    _gpsService();
     // timer = Timer.periodic(Duration(seconds: 1), (_) {
     //   setState(() {
     //     Prayer xx = Prayer.values.toList().firstWhere((element) => element.index == prayerTimes.nextPrayer().index);

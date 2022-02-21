2d040e8af324b2b06a22eda142ee12839e0ce582
mose98 <moussa98.mkg@gmail.com>
2022-02-22 00:31:21 +0100

    [mo][fixed] schedule notification

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -17,57 +17,72 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: Container(
-        height: MediaQuery.of(context).size.height * 0.90,
-        child: ListView(
-          children: [
-            loggedIn && userLogged.photoURL != null
-                ? Align(
-                    alignment: Alignment.center,
-                    child: CircleAvatarWithTransition(
-                      transitionBorderwidth: 20,
-                      primaryColor: Colors.blue,
-                      image: loggedIn && userLogged.photoURL != null
-                          ? NetworkImage(
-                              userLogged.photoURL!,
-                            )
-                          : NetworkImage(""),
-                    ),
-                  )
-                : Container(),
-            SizedBox(
-              height: 30,
-            ),
-            ThemeSetting(),
-            SizedBox(
-              height: 25,
-            ),
-            SchoolSetting(),
-            SizedBox(
-              height: 25,
-            ),
-            MethodSetting(),
-            SizedBox(
-              height: 25,
-            ),
-            GestureDetector(
-              onTap: () async {
-                await Authentication.signOut();
-                setState(() {
-                  loggedIn = false;
-                });
-              },
-              child: Text(
-                "SETTINGS\nclicca qui per fare il logout (solo test)\nNella pagina tracker si dovrà rifare il login",
-                textAlign: TextAlign.center,
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.90,
+          child: Stack(
+            children: [
+              GestureDetector(
+                onTap: (){
+                  Navigator.of(context).pop();
+                },
+                child: const Align(
+                  alignment: Alignment.topLeft,
+                  child: Icon(Icons.arrow_back_ios),
+                ),
               ),
-            ),
-          ],
+              ListView(
+                children: [
+                  loggedIn && userLogged.photoURL != null
+                      ? Align(
+                          alignment: Alignment.center,
+                          child: CircleAvatarWithTransition(
+                            transitionBorderwidth: 20,
+                            primaryColor: Colors.blue,
+                            image: loggedIn && userLogged.photoURL != null
+                                ? NetworkImage(
+                                    userLogged.photoURL!,
+                                  )
+                                : NetworkImage(""),
+                          ),
+                        )
+                      : Container(),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ThemeSetting(),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  SchoolSetting(),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  MethodSetting(),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  GestureDetector(
+                    onTap: () async {
+                      await Authentication.signOut();
+                      setState(() {
+                        loggedIn = false;
+                      });
+                    },
+                    child: Text(
+                      "SETTINGS\nclicca qui per fare il logout (solo test)\nNella pagina tracker si dovrà rifare il login",
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
-    ));
+      )),
+    );
   }
 }
